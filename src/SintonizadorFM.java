06d44e4c3142ccf4a6515f2c43e73b42b49b35f9
Elisabeth Maestre <elisabethmaestre9@gmail.com>
2024-11-22 23:28:44 +0100

    editado

--- src/SintonizadorFM.java
+++ src/SintonizadorFM.java
@@ -1,23 +1,24 @@
-public class SintonizadorFM {         // Clase SintonizadorFM que gestiona la frecuencia de una estación de radio.
-        private double frecuencia;        // Creamos la variable frecuencia de forma privada (para protegerla)
-
-        public SintonizadorFM() {         // Constructor con el mismo nombre que la clase (obligatorio) y público para acceder a el al crear un objeto.
-            frecuencia = 80;              // Inicializamos frecuencia con un valor por defecto de 80
+public class SintonizadorFM {
+        private double frecuencia;
+        //HOLA SOY ELISABET MAESTRE!!!
+        // ESTE ES EL CAMBIO
+        public SintonizadorFM() {
+            frecuencia = 99;
         }
         public SintonizadorFM(double frecuencia) {
-            if(frecuencia < 80)
-                this.frecuencia = 80;     // Asignar el valor 80 si la frecuencia es menor que 80 MHz.
-            else if (frecuencia > 108)    // Asignar el valor 108 si la frecuencia es mayor que 108 MHz.
+            if(frecuencia < 99)
+                this.frecuencia = 99;
+            else if (frecuencia > 108)
                 this.frecuencia = 108;
             else
-                this.frecuencia = frecuencia; // Asignar la frecuencia proporcionada si está en el rango válido.
+                this.frecuencia = frecuencia;
         }
 
         public void display() {               // Muestra la frecuencia actual.
             System.out.println("Frecuencia: " + frecuencia + " MHz");
         }
 
-        public void down() {                  // Baja la frecuencia 0.5 MHz y luego verifica si sigue dentro del rango permitido (salta al metodo comprobarRango).
+        public void down() {                
             frecuencia -= 0.5;
             comprobarRango();
         }
@@ -31,6 +32,6 @@ public class SintonizadorFM {         // Clase SintonizadorFM que gestiona la fr
             if(frecuencia < 80)
                 frecuencia = 108;            // Si la frecuencia es menor a 80, se ajusta a 108.
             else if (frecuencia > 108)
-                frecuencia = 80;             // Si la frecuencia es mayor a 108, se ajusta a 80.
+                frecuencia = 99;             // Si la frecuencia es mayor a 108, se ajusta a 80.
         }
     }
